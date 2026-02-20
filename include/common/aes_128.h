60ca1f2f721b37bdba18f135b7e5a903ca084f6c
Kieran O'Neill <hello@kieranoneill.com>
2026-02-20 11:07:42 +0100

    feat: updates implemnations to return an error code

--- include/common/aes_128.h
+++ include/common/aes_128.h
@@ -2,13 +2,13 @@
 
 #include <stdint.h>
 
-void aes_128_decrypt(
+int aes_128_decrypt(
     const uint8_t key[16],
     const uint8_t ciphertext[16],
     uint8_t plaintext[16]
 );
 
-void aes_128_encrypt(
+int aes_128_encrypt(
     const uint8_t key[16],
     const uint8_t plaintext[16],
     uint8_t ciphertext[16]
