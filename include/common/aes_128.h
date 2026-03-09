5f62a33db98bd91ede594f0cd33a76a056b445d1
Kieran O'Neill <hello@kieranoneill.com>
2026-03-09 19:06:13 +0100

    test: implements unit tests for each round/step and expected state of ciphertext

--- include/common/aes_128.h
+++ include/common/aes_128.h
@@ -2,14 +2,13 @@
 
 #include <stdint.h>
 
-int aes_128_decrypt(
-    const uint8_t key[16],
-    const uint8_t ciphertext[16],
-    uint8_t plaintext[16]
-);
-
+void add_round_key(uint8_t state[16], const uint8_t round_key[16]);
 int aes_128_encrypt(
     const uint8_t key[16],
     const uint8_t plaintext[16],
     uint8_t ciphertext[16]
 );
+void key_expansion(const uint8_t cipherkey[16], uint8_t key_schedules[11][16]);
+void mix_columns(uint8_t state[16]);
+void shift_rows(uint8_t state[16]);
+void sub_bytes(uint8_t state[16]);
