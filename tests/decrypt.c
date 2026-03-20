d197b725a1d08a3ab67b8a1ff56e07c9a3414719
Kieran O'Neill <hello@kieranoneill.com>
2026-03-20 14:04:27 +0100

    tests: moves testing utilities to separate directory and files

--- tests/decrypt.c
+++ tests/decrypt.c
@@ -3,7 +3,10 @@
 #include "aes_128.h"
 #include "unity.h"
 #include "sodium.h"
-#include "test_utils.h"
+#include "utilities/assertations.h"
+#include "utilities/constants.h"
+#include "utilities/macros.h"
+#include "utilities/types.h"
 
 /**
  * Test the full end-to-end functionality of AES-128 implementation using known ciphertext ->
--- tests/utilities/assertations.c
+++ tests/utilities/assertations.c
@@ -1,10 +1,6 @@
 #include "unity.h"
 #include "sodium.h"
-#include "test_utils.h"
-
-const char ciphertext_as_hex[33] = "3925841d02dc09fbdc118597196a0b32";
-const char key_as_hex[33] = "2b7e151628aed2a6abf7158809cf4f3c";
-const char plaintext_as_hex[33] = "3243f6a8885a308d313198a2e0370734";
+#include "assertations.h"
 
 void assert_round_state(
     const uint8_t expected[16],
