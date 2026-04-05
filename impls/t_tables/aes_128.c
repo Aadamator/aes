2938c99b0b411430e615ac9cfb7cb2a91d5060f1
Kieran O'Neill <hello@kieranoneill.com>
2026-04-05 10:14:29 +0200

    perf: moves key expansion outside block algorithm (#12)
    * feat: adds key expansion function as utility function
    
    * refactor: removes the key expansion from the encrypt function
    
    * test: pre-computes key schedules before encryption/decryption
    
    * build: moves the key expansion outsode of benchmarking encryption

--- impls/t_tables/aes_128.c
+++ impls/t_tables/aes_128.c
@@ -3,14 +3,14 @@
 
 void add_round_key(const uint8_t round_key[16], uint8_t state[16]) {}
 
-int decrypt(const uint8_t key[16], const uint8_t ciphertext[16], uint8_t plaintext[16]) {
+int decrypt(const uint8_t key_schedules[11][16], const uint8_t ciphertext[16], uint8_t plaintext[16]) {
     // placeholder, just returns the cipertext
     for (int i = 0; i < 16; ++i) plaintext[i] = ciphertext[i];
 
     return 0;
 }
 
-int encrypt(const uint8_t key[16], const uint8_t plaintext[16], uint8_t ciphertext[16]) {
+int encrypt(const uint8_t key_schedules[11][16], const uint8_t plaintext[16], uint8_t ciphertext[16]) {
     // placeholder, just returns the plaintext
     for (int i = 0; i < 16; ++i) ciphertext[i] = plaintext[i];
 
